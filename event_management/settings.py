a90ace7682bf3dce1540a30655aa75aaaad1224d
DevloperSiam <mdsiamh77@gmail.com>
2025-11-21 17:56:42 +0400

    Add DEFAULT_FROM_EMAIL config and email fixes

--- event_management/settings.py
+++ event_management/settings.py
@@ -167,6 +167,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
 
 FRONTEND_URL = 'https://events-managment-mgt5.onrender.com/'
